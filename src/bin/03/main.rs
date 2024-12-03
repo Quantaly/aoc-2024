f73b22d2aa1cd5819a996aa64f6d64a99a11af97
Kai Page <kai@quantaly.net>
2024-12-03 08:52:37 -0700

    Day 3

---
+++ src/bin/03/main.rs
@@ -0,0 +1,38 @@
+use anyhow::Result;
+use aoc_2024::input_string;
+use regex::Regex;
+use std::str::FromStr;
+
+fn main() -> Result<()> {
+    let mul_regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)")?;
+    let enabled_block_regex =
+        Regex::new(r"(?:^|do\(\))(?:[^d]|d[^o]|do[^n]|don[^']|don'[^t]|don't[^(]|don't\([^)])*")?;
+
+    let input = input_string()?;
+
+    let part_1_sum = mul_regex
+        .captures_iter(&input)
+        .map(|c| {
+            let (_, [a, b]) = c.extract();
+            let a = u32::from_str(a)?;
+            let b = u32::from_str(b)?;
+            Ok(a * b)
+        })
+        .sum::<Result<u32>>()?;
+    println!("{part_1_sum}");
+
+    let part_2_sum = enabled_block_regex
+        .find_iter(&input)
+        .flat_map(|enabled_block| {
+            mul_regex.captures_iter(enabled_block.as_str()).map(|c| {
+                let (_, [a, b]) = c.extract();
+                let a = u32::from_str(a)?;
+                let b = u32::from_str(b)?;
+                Ok(a * b)
+            })
+        })
+        .sum::<Result<u32>>()?;
+    println!("{part_2_sum}");
+
+    Ok(())
+}
